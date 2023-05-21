7f1a3e034bfd90013f6c3b0dad69b06990653919
dittoryanda <6182001056@student.unpar.ac.id>
2023-05-22 02:33:23 +0700

    22/05 2.:32am
    npm installations,
    convert login html to ejs,
    index.js

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const express = require('express');
+import express from 'express';
+import mysql from 'mysql';
+import session from 'express-session';
+import memoryStore from 'memorystore';
+import bodyParser from "body-parser";
+
 const app = express();
+const sessionStore = memoryStore(session);
+app.use(session({
+    cookie: {
+        httpOnly: false,
+        sameSite: 'strict',
+        maxAge: 1 * 60 * 60 * 1000
+    },
+    store: new sessionStore({
+        checkPeriod: 1 * 60 * 60 * 1000
+    }),
+    name: 'SID',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const pool = mysql.createPool({
+    user: 'root',
+    password: '',
+    database: 'tubes_privdat',
+    host: 'localhost',
+    connectionLimit: 10,
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.render('login');
 });
 
 app.post('/', (req, res) => {
