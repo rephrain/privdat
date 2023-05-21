d4bdd2f341dbe6fa910aa34e8ea42d02b10a4619
dittoryanda <6182001056@student.unpar.ac.id>
2023-05-22 04:05:46 +0700

    22/05 4.04
    role officer,
    index.js get post,
    login activity officer,
    form applicant ejs dummy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mysql from 'mysql';
 import session from 'express-session';
 import memoryStore from 'memorystore';
 import bodyParser from "body-parser";
+import crypto from 'crypto';
 
 const app = express();
 const sessionStore = memoryStore(session);
@@ -37,8 +38,35 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    const name = req.body.name;
-    res.send(`Hello, ${name}!`);
+    let username = req.body.username
+    let password = req.body.password
+    const hashed_pass = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
+    pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, hashed_pass], (err, result) => {
+        if (result.length > 0){
+            req.session.username = username;
+            //login as admin
+            if (result[0].role === 1) {
+                req.session.role = 'officer';               
+                res.redirect('/officer/home');
+            }
+        }
+        else{
+            res.send(hashed_pass)
+        }
+    })
+});
+
+app.get('/officer/home', (req, res) => {
+
+    res.render('home_officer');
+});
+
+app.get('/applicant_form', (req, res) => {
+
+    res.render('form1_applicant');
 });
 
 app.listen(8080);
